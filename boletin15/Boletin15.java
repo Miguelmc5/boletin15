643b7ede3fe2ee6a015c285ca30cd3380167467a
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2020-01-16 14:32:47 +0100

    main

---
+++ boletin15/Boletin15.java
@@ -0,0 +1,19 @@
+package boletin15;
+
+/**
+ *
+ * @author DANIELCASTELAO\mmartinezcosta
+ */
+public class Boletin15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        Academica alumno=new Academica();
+        alumno.Acceso();
+    }
+    
+}

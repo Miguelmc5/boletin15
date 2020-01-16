f1a853715127ee79ec155fd01679be8a2d7604ec
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2020-01-16 14:33:25 +0100

    persoal

---
+++ paquete2/Persoal.java
@@ -0,0 +1,31 @@
+package paquete2;
+
+/**
+ *
+ * @author mmartinezcosta
+ */
+public class Persoal {
+
+    private int telefono;
+    private String correo;
+    
+    public Persoal(){
+        
+    }
+
+    public Persoal(int telefono, String correo) {
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+    
+    public void Acceso(){
+        
+    }
+    
+    @Override
+    toString()
+    
+    
+    
+}
+
